2c02a5aaefa7939ce20d63cbabd29aaa800cc6bd
Charles Cruse <charle9@vt.edu>
2023-02-28 16:04:28 -0500

    finished MyMap

--- skeleton/skeleton/phase_2_enumerable.js
+++ skeleton/skeleton/phase_2_enumerable.js
@@ -3,3 +3,34 @@ Array.prototype.myEach = function(callback) {
         callback(this[i])
     }
 }
+
+Array.prototype.myEach = function(callback) {
+    for (let ele of this) {
+        callback(ele)
+    }
+}
+
+Array.prototype.myMap = function(callback) {
+    let a = []
+
+    this.myEach(ele => a.push(callback(ele)))
+    return a; 
+}
+
+// [1,2,3].myMap( ele => {
+//     return ele*2
+
+// [1,2,3]myMap( ele => (
+//     ele*2
+// ));
+// These are the same result
+
+
+Array.prototype.myReduce = function(callback, initialValue=this[0]) {
+    
+    acc = this.myEach(ele => {
+        return callback(acc, ele)
+    })
+
+    return acc
+}
